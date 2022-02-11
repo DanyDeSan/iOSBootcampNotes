736abe161fbe25142b8c99efca25528998ba15b4
Dany De San <ldanieldesanpedro@gmail.com>
2022-02-11 02:19:41 -0600

    Added crypto checker

---
+++ Proyectos/CryptoChecker/CryptoChecker/CryptoViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoChecker
+//
+//  Created by L Daniel De San Pedro on 11/02/22.
+//
+
+import UIKit
+
+class CryptoViewController: UIViewController {
+    
+    var collectionView: UICollectionView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
---
+++ Proyectos/CryptoChecker/CryptoChecker/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  CryptoChecker
+//
+//  Created by L Daniel De San Pedro on 11/02/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = scene as? UIWindowScene else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = scene
+        window?.rootViewController = CryptoViewController()
+        window?.makeKeyAndVisible()
+    }
+
+}
+
