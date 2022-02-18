2457c19f755f88692611ae137aeffeb0a6f9e145
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-02-17 18:09:54 -0600

    first commit

--- SOLID/Interface_Segregation_Principle.playground/Contents.swift
+++ SOLID/Interface_Segregation_Principle.playground/Contents.swift
@@ -1,3 +1,8 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Interface segregation principle o Principio de segregación de la interfaz
+//El Principio de segregación de la interfaz indica que es mejor tener diferentes interfaces (protocolos) que sean específicos para cada cliente, que tener un interfaz general. Además, indica que un cliente no tendría que implementar métodos que no utilice.
+//
+//Por ejemplo, podemos crear una interfaz para animales que incluya métodos de desplazamiento:
+//
+
